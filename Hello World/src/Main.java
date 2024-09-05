7388973b188da7d9bd5d9478d28775ce26fb936e
jaflanet <jonathan.aurelius@ui.ac.id>
2024-09-05 11:56:45 +0700

    odd even loop

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -42,14 +42,16 @@ public class Main {
 
 
     public static void main(String[] args) {
+        int i=1;
 
-        for (int i = 1; i <= 50; i++) {
+        while (i<=50) {
             if (i%2 == 0){
                 System.out.print(" even ");
             }
             else{
                 System.out.print(i);
             }
+            i++;
         }
     }
 }

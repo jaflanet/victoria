e6bbf7e93839781f86616959174f41f7568af2c8
jaflanet <jonathan.aurelius@ui.ac.id>
2024-09-05 10:27:45 +0700

    hello world

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -3,7 +3,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World !");
+        int num1;
+        long num2;
+        float num3;
+        double num4;
+        char car1;
+        boolean is1;
+
+        num1= 1;
+        num2= 2;
+        num3= 3.4F ;
+        num4= 3.5;
+        car1 = a;
+        is1 = True;
+
+
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(car1);
 
     }
 }

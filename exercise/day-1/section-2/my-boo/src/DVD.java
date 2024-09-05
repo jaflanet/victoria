b2bec05474831ce28f1665fe97e388fd9e9db5a2
jaflanet <jonathan.aurelius@ui.ac.id>
2024-09-05 20:46:59 +0700

    make dvd java

--- exercise/day-1/section-2/my-boo/src/DVD.java
+++ exercise/day-1/section-2/my-boo/src/DVD.java
@@ -1,2 +1,37 @@
 public class DVD extends LibraryItem{
+    private String director;
+    private int duration;
+
+
+    public DVD(String title, String itemId, String author, int pages) {
+        super(title, itemId);
+        this.director = author;
+        this.duration = pages;
+    }
+
+    @Override
+    public void checkOut(){
+        if(!isCheckedOut){
+            isCheckedOut=true;
+        }
+    }
+
+    @Override
+    public void returnItem(){
+        if(isCheckedOut){
+            isCheckedOut=false;
+        }
+    }
+
+    @Override
+    public void printDetails(){
+        System.out.println(" ");
+        System.out.println("DVD Title : " + title);
+        System.out.println("Director : " + director);
+        System.out.println("Duration : " + duration);
+        System.out.println("Check Out :" + isCheckedOut);
+    }
+
 }
+
+

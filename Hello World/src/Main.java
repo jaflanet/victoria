b4494cf1ef8dc656c0dbd28ee3c3dbf36d59cabb
jaflanet <jonathan.aurelius@ui.ac.id>
2024-09-05 10:51:00 +0700

    luas lingkaran

--- Hello World/src/Main.java
+++ Hello World/src/Main.java
@@ -1,28 +1,46 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static final float phi = 3.14f ;
+    static float r,l1,l2;
+
+    static float luas( float r) {
+        float l =r*r*phi;
+        return l;
+    }
+
     public static void main(String[] args) {
 
-        int num1;
-        long num2;
-        float num3;
-        double num4;
-        char car1;
-        boolean is1;
+        r=10f;
+        l1=luas(r);
+        r= 15f;
+        l2=luas(r);
 
-        num1= 1;
-        num2= 2;
-        num3= 3.4F ;
-        num4= 3.5;
-        car1 = 'a';
-        is1 = true;
+        System.out.println(l1);
+        System.out.println(l2);
+//        int num1;
+//        long num2;
+//        float num3;
+//        double num4;
+//        char car1;
+//        boolean is1;
+//
+//        num1= 1;
+//        num2= 2;
+//        num3= 3.4F ;
+//        num4= 3.5;
+//        car1 = 'a';
+//        is1 = true;
+//
+//        System.out.println(num1);
+//        System.out.println(num2);
+//        System.out.println(num3);
+//        System.out.println(num4);
+//        System.out.println(car1);
+//        System.out.println(is1);
 
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(num3);
-        System.out.println(num4);
-        System.out.println(car1);
-        System.out.println(is1);
+
+
 
     }
 }
